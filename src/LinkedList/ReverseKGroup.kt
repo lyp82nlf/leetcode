2c030dcd31c3a6b2a2944797b1ed35b15c90a29f
qiuling <qiuling@baijiayun.com>
2021-01-18 23:41:29 +0800

    feat 25. K 个一组翻转链表

--- src/LinkedList/ReverseKGroup.kt
+++ src/LinkedList/ReverseKGroup.kt
@@ -7,17 +7,35 @@ import Utils.ListNode
  */
 class ReverseKGroup {
     fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
-        var remainK = k
-        var current = head
-        var next = current?.next
-        if (current == null || next == null) {
+        var pre: ListNode? = null
+        var cur = head
+        var next = cur?.next
+        var tmpK = k
+        val canArriveK = canArriveK(head, k)
+        if (canArriveK) {
+            while (cur != null && tmpK > 0) {
+                cur.next = pre
+                pre = cur
+                cur = next
+                next = next?.next
+                tmpK--
+            }
+        } else {
             return head
         }
-        while (remainK > 0) {
-            var node =  next.next
-            next.next = current
-
-        }
-        head.next = reverseKGroup(next.next,k)
+        head?.next = reverseKGroup(cur, k)
+        return pre
     }
+
+    fun canArriveK(head: ListNode?, k: Int): Boolean {
+        var head = head
+        var k = k
+        while (k > 1) {
+            head = head?.next
+            k--
+        }
+        return head != null
+    }
+
+
 }
